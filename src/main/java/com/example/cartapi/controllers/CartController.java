fc320aefd6190e4c0c81b551475305b621809c7a
n-marcelino <nicomarcelino0613@gmail.com>
2024-07-02 01:02:01 +0800

    initial commit

---
+++ src/main/java/com/example/cartapi/controllers/CartController.java
@@ -0,0 +1,84 @@
+package com.example.cartapi.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.example.cartapi.models.CartItem;
+
+@RestController
+@RequestMapping("api/cart")
+public class CartController {
+    private final List<CartItem> cartItems = new ArrayList<>();
+    private final AtomicInteger counter = new AtomicInteger();
+
+    //get all cart items
+    @GetMapping
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    //get cart item by id
+    @GetMapping("/{id}")
+    public ResponseEntity<CartItem> getCartItemById(@PathVariable int id) {
+        Optional<CartItem> cartItem = cartItems.stream().filter(c -> c.getId() == id).findFirst();
+        return cartItem.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    //add new cart item
+    @PostMapping
+    public ResponseEntity<CartItem> addCartItem(@RequestBody CartItem cartItem) {
+        cartItem.setId(counter.incrementAndGet());
+        cartItems.add(cartItem);
+        return new ResponseEntity<>(cartItem, HttpStatus.CREATED);
+    }
+
+    //add new cart item through headers
+    @PostMapping("/add")
+    public ResponseEntity<CartItem> addCartItemWithHeader(@RequestHeader String name, @RequestHeader int quantity, @RequestHeader BigDecimal price) {
+        CartItem cartItem = new CartItem();
+
+        cartItem.setId(counter.incrementAndGet());
+        cartItem.setName(name);
+        cartItem.setQuantity(quantity);
+        cartItem.setPrice(price);
+        cartItems.add(cartItem);
+
+        return new ResponseEntity<>(cartItem, HttpStatus.CREATED);
+    }
+
+    //update existing cart item
+    @PutMapping("/{id}")
+    public ResponseEntity<CartItem> updateCartItem(@PathVariable int id, @RequestBody CartItem cartItemDetails) {
+        Optional<CartItem> cartItemOptional = cartItems.stream().filter(c -> c.getId() == id).findFirst();
+        if (cartItemOptional.isPresent()) {
+            CartItem cartItem = cartItemOptional.get();
+            cartItem.setName(cartItemDetails.getName());
+            cartItem.setQuantity(cartItemDetails.getQuantity());
+            cartItem.setPrice(cartItemDetails.getPrice());
+            return ResponseEntity.ok(cartItem);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    //delete cart item
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable int id) {
+        Optional<CartItem> cartItemOptional = cartItems.stream().filter(c -> c.getId() == id).findFirst();
+        if (cartItemOptional.isPresent()) {
+            cartItems.remove(cartItemOptional.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/main/java/com/example/cartapi/models/CartItem.java
@@ -0,0 +1,43 @@
+package com.example.cartapi.models;
+
+import java.math.BigDecimal;
+
+public class CartItem {
+    private int id;
+    private String name;
+    private int quantity;
+    private BigDecimal price;
+
+    //getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
